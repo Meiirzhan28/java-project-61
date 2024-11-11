b7242a22afb0339ab1e2f1648fe9303cb3728d98
Meiirzhan28 <meirjan28@mail.ru>
2024-11-11 17:13:46 +0500

    final

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -6,7 +6,7 @@ public class Utils {
     private static final int MAX = 101;
     private static final Random RAND = new Random();
     public static int random(int min, int max) {
-        return RAND.nextInt((max - min) + 1) + min;
+        return RAND.nextInt(min, max);
     }
     public static int random() {
         return RAND.nextInt(MAX);
